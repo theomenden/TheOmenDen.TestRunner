bff6f96bd6a60426f06a98388c478e92913a95a9
Andrew Beers <andrew.d.beers@gmail.com>
2022-08-06 19:27:53 -0400

    Add project files.

---
+++ TheOmenDen.TestRunner/Program.cs
@@ -0,0 +1,52 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.Bootstrap;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Serilog;
+using Serilog.Events;
+using TheOmenDen.TestRunner;
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+    .Enrich.FromLogContext()
+    .Enrich.WithThreadId()
+    .Enrich.WithProcessName()
+    .Enrich.WithEnvironmentUserName()
+    .Enrich.WithMemoryUsage()
+    .WriteTo.Async(a =>
+    {
+        a.File("./logs/log-.txt", rollingInterval: RollingInterval.Day);
+        a.Console();
+    })
+    .CreateBootstrapLogger();
+
+try
+{
+
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+    builder.Services
+        .AddBlazorise(options =>
+        {
+            options.Immediate = true;
+        })
+        .AddBootstrap5Providers()
+        .AddBootstrap5Components()
+        .AddBootstrapIcons();
+
+    await using var app = builder.Build();
+
+    await app.RunAsync();
+}
+catch(Exception ex)
+{
+    Log.Fatal("The test runner failed to start due to: {@Ex}", ex);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
